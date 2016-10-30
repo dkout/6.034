1b242ebccd272716a698ad6e7247d72659fdb9b2
Jessica Noss <jmn@mit.edu>
2016-10-30 16:29:01 -0400

    updated tests to match part 2 updated spec

--- tests.py
+++ tests.py
@@ -393,59 +393,72 @@ make_test(type = 'FUNCTION_ENCODED_ARGS',
 
 
 ## update_svm_from_alphas
+update_svm_from_alphas_0_input = svm_update_recit_0.copy().set_boundary([], 0)
+update_svm_from_alphas_0_input.support_vectors = []
 def update_svm_from_alphas_0_getargs() :  #TEST 34
-    svm = svm_update_recit_0.copy().set_boundary([], 0)
-    svm.support_vectors = []
-    return [svm]
+    return [update_svm_from_alphas_0_input]
 def update_svm_from_alphas_0_testanswer(val, original_val = None) :
-    return val == svm_update_recit_0
+    return val == svm_update_recit_0 and val is update_svm_from_alphas_0_input
 make_test(type = 'FUNCTION_ENCODED_ARGS',
           getargs = update_svm_from_alphas_0_getargs,
           testanswer = update_svm_from_alphas_0_testanswer,
-          expected_val = str(svm_update_recit_0),
+          expected_val = 'Input ' + str(svm_update_recit_0),
           name = 'update_svm_from_alphas')
 
+update_svm_from_alphas_1_input = svm_update_recit_1.copy().set_boundary([52, 19], 6)
+update_svm_from_alphas_1_input.support_vectors = [Point('E', [3, 2], -1, 0)]
 def update_svm_from_alphas_1_getargs() :  #TEST 35
-    svm = svm_update_recit_1.copy().set_boundary([52, 19], 6)
-    svm.support_vectors = [Point('E', [3, 2], -1, 0)]
-    return [svm]
+    return [update_svm_from_alphas_1_input]
 def update_svm_from_alphas_1_testanswer(val, original_val = None) :
-    return val == svm_update_recit_1
+    return val == svm_update_recit_1 and val is update_svm_from_alphas_1_input
 make_test(type = 'FUNCTION_ENCODED_ARGS',
           getargs = update_svm_from_alphas_1_getargs,
           testanswer = update_svm_from_alphas_1_testanswer,
-          expected_val = str(svm_update_recit_1),
+          expected_val = 'Input ' + str(svm_update_recit_1),
           name = 'update_svm_from_alphas')
 
+update_svm_from_alphas_2_input = svm_update_recit_2.copy().set_boundary([1.0, 0.0], -1.5)
+update_svm_from_alphas_2_input.support_vectors = [Point('B', [1, 1], 1, 1.0)]
 def update_svm_from_alphas_2_getargs() :  #TEST 36
-    svm = svm_update_recit_2.copy().set_boundary([1.0, 0.0], -1.5)
-    svm.support_vectors = [Point('B', [1, 1], 1, 1.0)]
-    return [svm]
+    return [update_svm_from_alphas_2_input]
 def update_svm_from_alphas_2_testanswer(val, original_val = None) :
-    return val == svm_update_recit_2
+    return val == svm_update_recit_2 and val is update_svm_from_alphas_2_input
 make_test(type = 'FUNCTION_ENCODED_ARGS',
           getargs = update_svm_from_alphas_2_getargs,
           testanswer = update_svm_from_alphas_2_testanswer,
-          expected_val = str(svm_update_recit_2),
+          expected_val = 'Input ' + str(svm_update_recit_2),
           name = 'update_svm_from_alphas')
 
+update_svm_from_alphas_3_input = svm_update_recit_1.copy()
 def update_svm_from_alphas_3_getargs() :  #TEST 37
-    return [svm_update_recit_1.copy()]
+    return [update_svm_from_alphas_3_input]
 def update_svm_from_alphas_3_testanswer(val, original_val = None) :
-    return val == svm_update_recit_1
+    return val == svm_update_recit_1 and val is update_svm_from_alphas_3_input
 make_test(type = 'FUNCTION_ENCODED_ARGS',
           getargs = update_svm_from_alphas_3_getargs,
           testanswer = update_svm_from_alphas_3_testanswer,
-          expected_val = str(svm_update_recit_1),
+          expected_val = 'Input ' + str(svm_update_recit_1),
+          name = 'update_svm_from_alphas')
+
+update_svm_from_alphas_4_input = svm_update_harvard_mit_n.copy().set_boundary([], 0)
+update_svm_from_alphas_4_input.support_vectors = []
+def update_svm_from_alphas_4_getargs() :  #TEST 38
+    return [update_svm_from_alphas_4_input]
+def update_svm_from_alphas_4_testanswer(val, original_val = None) :
+    return val == svm_update_harvard_mit_n and val is update_svm_from_alphas_4_input
+make_test(type = 'FUNCTION_ENCODED_ARGS',
+          getargs = update_svm_from_alphas_4_getargs,
+          testanswer = update_svm_from_alphas_4_testanswer,
+          expected_val = 'Input ' + str(svm_update_harvard_mit_n),
           name = 'update_svm_from_alphas')
 
 
 #ANSWER_1
-ANSWER_1_getargs = 'ANSWER_1'  #TEST 38
+ANSWER_1_getargs = 'ANSWER_1'  #TEST 39
 def ANSWER_1_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
-    return val == 18
+    return val == 11
 make_test(type = 'VALUE',
           getargs = ANSWER_1_getargs,
           testanswer = ANSWER_1_testanswer,
@@ -453,7 +466,7 @@ make_test(type = 'VALUE',
           name = ANSWER_1_getargs)
 
 #ANSWER_2
-ANSWER_2_getargs = 'ANSWER_2'  #TEST 39
+ANSWER_2_getargs = 'ANSWER_2'  #TEST 40
 def ANSWER_2_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -465,7 +478,7 @@ make_test(type = 'VALUE',
           name = ANSWER_2_getargs)
 
 #ANSWER_3
-ANSWER_3_getargs = 'ANSWER_3'  #TEST 40
+ANSWER_3_getargs = 'ANSWER_3'  #TEST 41
 def ANSWER_3_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -477,7 +490,7 @@ make_test(type = 'VALUE',
           name = ANSWER_3_getargs)
 
 #ANSWER_4
-ANSWER_4_getargs = 'ANSWER_4'  #TEST 41
+ANSWER_4_getargs = 'ANSWER_4'  #TEST 42
 def ANSWER_4_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -489,7 +502,7 @@ make_test(type = 'VALUE',
           name = ANSWER_4_getargs)
 
 #ANSWER_5
-ANSWER_5_getargs = 'ANSWER_5'  #TEST 42
+ANSWER_5_getargs = 'ANSWER_5'  #TEST 43
 def ANSWER_5_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -501,7 +514,7 @@ make_test(type = 'VALUE',
           name = ANSWER_5_getargs)
 
 #ANSWER_6
-ANSWER_6_getargs = 'ANSWER_6'  #TEST 43
+ANSWER_6_getargs = 'ANSWER_6'  #TEST 44
 def ANSWER_6_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -513,7 +526,7 @@ make_test(type = 'VALUE',
           name = ANSWER_6_getargs)
 
 #ANSWER_7
-ANSWER_7_getargs = 'ANSWER_7'  #TEST 44
+ANSWER_7_getargs = 'ANSWER_7'  #TEST 45
 def ANSWER_7_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -525,11 +538,11 @@ make_test(type = 'VALUE',
           name = ANSWER_7_getargs)
 
 #ANSWER_8
-ANSWER_8_getargs = 'ANSWER_8'  #TEST 45
+ANSWER_8_getargs = 'ANSWER_8'  #TEST 46
 def ANSWER_8_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
-    return set(val) == set('A')
+    return val == []
 make_test(type = 'VALUE',
           getargs = ANSWER_8_getargs,
           testanswer = ANSWER_8_testanswer,
@@ -537,7 +550,7 @@ make_test(type = 'VALUE',
           name = ANSWER_8_getargs)
 
 #ANSWER_9
-ANSWER_9_getargs = 'ANSWER_9'  #TEST 46
+ANSWER_9_getargs = 'ANSWER_9'  #TEST 47
 def ANSWER_9_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -549,7 +562,7 @@ make_test(type = 'VALUE',
           name = ANSWER_9_getargs)
 
 #ANSWER_10
-ANSWER_10_getargs = 'ANSWER_10'  #TEST 47
+ANSWER_10_getargs = 'ANSWER_10'  #TEST 48
 def ANSWER_10_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -561,7 +574,7 @@ make_test(type = 'VALUE',
           name = ANSWER_10_getargs)
 
 #ANSWER_11
-ANSWER_11_getargs = 'ANSWER_11'  #TEST 48
+ANSWER_11_getargs = 'ANSWER_11'  #TEST 49
 def ANSWER_11_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -573,7 +586,7 @@ make_test(type = 'VALUE',
           name = ANSWER_11_getargs)
 
 #ANSWER_12
-ANSWER_12_getargs = 'ANSWER_12'  #TEST 49
+ANSWER_12_getargs = 'ANSWER_12'  #TEST 50
 def ANSWER_12_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -585,7 +598,7 @@ make_test(type = 'VALUE',
           name = ANSWER_12_getargs)
 
 #ANSWER_13
-ANSWER_13_getargs = 'ANSWER_13'  #TEST 50
+ANSWER_13_getargs = 'ANSWER_13'  #TEST 51
 def ANSWER_13_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -597,7 +610,7 @@ make_test(type = 'VALUE',
           name = ANSWER_13_getargs)
 
 #ANSWER_14
-ANSWER_14_getargs = 'ANSWER_14'  #TEST 51
+ANSWER_14_getargs = 'ANSWER_14'  #TEST 52
 def ANSWER_14_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -609,7 +622,7 @@ make_test(type = 'VALUE',
           name = ANSWER_14_getargs)
 
 #ANSWER_15
-ANSWER_15_getargs = 'ANSWER_15'  #TEST 52
+ANSWER_15_getargs = 'ANSWER_15'  #TEST 53
 def ANSWER_15_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -621,7 +634,7 @@ make_test(type = 'VALUE',
           name = ANSWER_15_getargs)
 
 #ANSWER_16
-ANSWER_16_getargs = 'ANSWER_16'  #TEST 53
+ANSWER_16_getargs = 'ANSWER_16'  #TEST 54
 def ANSWER_16_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -633,11 +646,11 @@ make_test(type = 'VALUE',
           name = ANSWER_16_getargs)
 
 #ANSWER_17
-ANSWER_17_getargs = 'ANSWER_17'  #TEST 54
+ANSWER_17_getargs = 'ANSWER_17'  #TEST 55
 def ANSWER_17_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
-    return set(val) == set([1,3,6,8])
+    return set(val) == set([1,3,8])
 make_test(type = 'VALUE',
           getargs = ANSWER_17_getargs,
           testanswer = ANSWER_17_testanswer,
@@ -645,7 +658,7 @@ make_test(type = 'VALUE',
           name = ANSWER_17_getargs)
 
 #ANSWER_18
-ANSWER_18_getargs = 'ANSWER_18'  #TEST 55
+ANSWER_18_getargs = 'ANSWER_18'  #TEST 56
 def ANSWER_18_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -657,7 +670,7 @@ make_test(type = 'VALUE',
           name = ANSWER_18_getargs)
 
 #ANSWER_19
-ANSWER_19_getargs = 'ANSWER_19'  #TEST 56
+ANSWER_19_getargs = 'ANSWER_19'  #TEST 57
 def ANSWER_19_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
@@ -669,7 +682,7 @@ make_test(type = 'VALUE',
           name = ANSWER_19_getargs)
 
 #ANSWER_20
-ANSWER_20_getargs = 'ANSWER_20'  #TEST 57
+ANSWER_20_getargs = 'ANSWER_20'  #TEST 58
 def ANSWER_20_testanswer(val, original_val = None):
     if val == None:
         raise NotImplementedError
